40f7ebd3bfe94187e2ca46b33a984f7b7ff2b6a3
Kevin <kevinhmai@outlook.com>
2019-11-27 22:41:27 -0800

    initial commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mclovin/Desktop/homework6/sudoku
+# Build directory: /home/mclovin/Desktop/homework6/sudoku/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../lib/googletest/googletest-build")
+subdirs("test")
---
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mclovin/Desktop/homework6/sudoku/test
+# Build directory: /home/mclovin/Desktop/homework6/sudoku/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit "/home/mclovin/Desktop/homework6/sudoku/cmake-build-debug/unit_test")
+set_tests_properties(unit PROPERTIES  _BACKTRACE_TRIPLES "/home/mclovin/Desktop/homework6/sudoku/test/CMakeLists.txt;12;add_test;/home/mclovin/Desktop/homework6/sudoku/test/CMakeLists.txt;0;")

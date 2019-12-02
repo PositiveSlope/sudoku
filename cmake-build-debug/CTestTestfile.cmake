188d1ae07450f857a3c35c9136bcc3b3846af349
Kevin <kevinhmai@outlook.com>
2019-12-01 18:26:44 -0800

    final commit

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mclovin/Desktop/homework6/sudoku
-# Build directory: /home/mclovin/Desktop/homework6/sudoku/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../lib/googletest/googletest-build")
-subdirs("test")
